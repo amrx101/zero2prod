d4e3bf295ac554f2196389e9afb26b1d55ef92c9
Amit Kumar <amit@turntide.com>
2023-05-20 12:47:35 +0530

    Insert into subscriptions table using Connection Pool

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
+use sqlx::PgPool;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Faile to read config");
-    let connection = PgConnection::connect(
+    let pool = PgPool::connect(
         &configuration.database.connection_string()
     ).await.expect("Falied to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,19 +1,19 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
-use sqlx::PgConnection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 
-pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, pool: PgPool) -> Result<Server, std::io::Error> {
 
-    let connection = web::Data::new(connection);
+    let db_pool = web::Data::new(pool);
 
     let server = HttpServer::new(move || {
         App::new()
         .route("/health_check", web::get().to(health_check))
         .route("/subscriptions", web::post().to(subscribe))
-        .app_data(connection.clone())
+        .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();

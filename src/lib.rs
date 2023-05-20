597f82f8bb1aca829bbbe2b9fddd5380b170d277
Amit Kumar <amit@turntide.com>
2023-05-20 13:21:39 +0530

    Basic subscriptions is up

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,4 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
-use actix_web::dev::Server;
-use std::net::TcpListener;
 pub mod configuration;
 pub mod routes;
 pub mod startup;
-// pub mod startup;
 
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name);
-    HttpResponse::Ok()
-}
-
-
-async fn health_check(req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
-
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse{
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new( || {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-        .listen(listener)?
-        .run();
-
-    Ok(server)
-}

f367d5a54d108caa686495bf885ad60406ebbdbd
Amit Kumar <amit@turntide.com>
2023-07-02 12:13:07 +0530

    Tracing and logging

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
+// use zero2prod::telemetry::{get_subscriber, init_subscriber};
 use std::net::TcpListener;
 use sqlx::PgPool;
+use env_logger::Env;
 
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
 
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
-    init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Faile to read config");
+    // let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    // init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("Failed to read config");
     let pool = PgPool::connect(
         &configuration.database.connection_string()
     ).await.expect("Falied to connect to Postgres");

13ee83135aeafdeef0bb7d2d8027c5d60edfff5b
Amit Kumar <amit@turntide.com>
2023-07-04 15:53:11 +0530

    name validation compledted

--- src/domain.rs
+++ src/domain.rs
@@ -3,6 +3,7 @@
 use std::collections::{HashMap, HashSet};
 use unicode_segmentation::UnicodeSegmentation;
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 pub struct NewSubscriber {
@@ -25,7 +26,7 @@ impl SubscriberName {
         &self.0
     }
 
-    pub fn parse(s: String) -> SubscriberName{
+    pub fn parse(s: String) -> Result<SubscriberName, String>{
 
         let is_empty_or_whitespace = s.trim().is_empty();
 
@@ -39,9 +40,9 @@ impl SubscriberName {
 
         let contains_forbidden = s.chars().any(|g| forbidden_chars.contains(&g));
         if is_too_long || is_empty_or_whitespace || contains_forbidden {
-            panic!("{} is not a valid subscriber name.", s);
+            Err(format!("{} is not a valid subscriber name.", s))
         } else {
-            Self(s)
+            Ok(Self(s))
         }
     }
 }
@@ -50,4 +51,18 @@ impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
     }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claims::{assert_err, assert_ok};
+
+    #[test]
+    fn name_longer_than_256_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
 }

46922c6589ee65cd0112895d8f5e601bfc866a53
Amit Kumar <amit@turntide.com>
2023-07-08 21:13:53 +0530

    Rudimentary send_email

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,17 +1,18 @@
-use actix_web::{web, HttpResponse};
-use sqlx::PgPool;
-use uuid::Uuid;
+use actix_web::{trace, web, HttpResponse};
 use chrono::Utc;
+use sqlx::PgPool;
 use std::convert::{TryFrom, TryInto};
+use uuid::Uuid;
 
-use crate::domain::subscriber_name::SubscriberName;
-use crate::domain::subscriber_email::SubscriberEmail;
 use crate::domain::new_subscriber::NewSubscriber;
+use crate::domain::subscriber_email::SubscriberEmail;
+use crate::domain::subscriber_name::SubscriberName;
+use crate::email_client::EmailClient;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
-    name: String
+    name: String,
 }
 
 impl TryFrom<FormData> for NewSubscriber {
@@ -20,13 +21,13 @@ impl TryFrom<FormData> for NewSubscriber {
     fn try_from(value: FormData) -> Result<Self, Self::Error> {
         let name = SubscriberName::parse(value.name)?;
         let email = SubscriberEmail::parse(value.email)?;
-        Ok(Self{email, name})
+        Ok(Self { email, name })
     }
 }
 
 #[tracing::instrument(
     name="Adding a new subscriber",
-    skip(form, pool),
+    skip(form, pool, email_client),
     fields(
         subsciber_email=%form.email,
         subscriber_name=%form.name
@@ -34,19 +35,37 @@ impl TryFrom<FormData> for NewSubscriber {
 )]
 pub async fn subscribe(
     form: web::Form<FormData>,
-    pool: web::Data<PgPool>, ) -> HttpResponse {
-
+    pool: web::Data<PgPool>,
+    email_client: web::Data<EmailClient>,
+) -> HttpResponse {
     let new_subscriber = match form.0.try_into() {
         Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    match insert_subscriber(&pool, &new_subscriber).await
-    {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish()
+    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
     }
+    if send_confirmation_email(&email_client, new_subscriber)
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish();
+    }
+    HttpResponse::Ok().finish()
 }
+#[tracing::instrument(
+    name = "Send a confimation email to a new subscriber",
+    skip(email_client, new_subscriber)
+)]
 
+pub async fn send_confirmation_email(
+    email_client: &EmailClient,
+    new_subscriber: NewSubscriber,
+) -> Result<(), reqwest::Error> {
+    email_client
+        .send_email(new_subscriber.email, "Welcome", "Welcome", "Welcome")
+        .await
+}
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
@@ -54,23 +73,23 @@ pub async fn subscribe(
 )]
 pub async fn insert_subscriber(
     pool: &PgPool,
-    new_subscriber: &NewSubscriber
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
-    sqlx::query!( r#"
-        INSERT INTO subscriptions (id, email, name, subscribed_at)
-        VALUES ($1, $2, $3, $4)
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at, status)
+        VALUES ($1, $2, $3, $4, 'pending_confirrmation')
         "#,
         Uuid::new_v4(),
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
-        .execute(pool)
-        .await
-        .map_err(|e| {
-            tracing::error!("Failed to execute query: {:?}", e);
-            e
-        })?;
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("Failed to execute query: {:?}", e);
+        e
+    })?;
     Ok(())
-
 }

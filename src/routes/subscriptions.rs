e34f025710080a888ea0c5adece74164e1a3409e
Amit Kumar <amit@turntide.com>
2023-07-09 11:37:37 +0530

    WIP

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,5 +1,7 @@
 use actix_web::{trace, web, HttpResponse};
 use chrono::Utc;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 use sqlx::PgPool;
 use std::convert::{TryFrom, TryInto};
 use std::sync::Arc;
@@ -45,10 +47,14 @@ pub async fn subscribe(
         Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
-        return HttpResponse::InternalServerError().finish();
-    }
-    if send_confirmation_email(&email_client, new_subscriber, &base_url, "myToken")
+
+    let subscriber_id = match insert_subscriber(&pool, &new_subscriber) {
+        Ok(subscriber_id) => subscriber_id,
+        Err(_) => return HttpResponse::InternalServerError().finish(),
+    };
+
+    let subscriber_token = generate_subscription_token();
+    if send_confirmation_email(&email_client, new_subscriber, &base_url, &subscriber_token)
         .await
         .is_err()
     {
@@ -56,11 +62,11 @@ pub async fn subscribe(
     }
     HttpResponse::Ok().finish()
 }
+
 #[tracing::instrument(
     name = "Send a confimation email to a new subscriber",
     skip(email_client, new_subscriber, base_url, subscription_token)
 )]
-
 pub async fn send_confirmation_email(
     email_client: &EmailClient,
     new_subscriber: NewSubscriber,
@@ -95,13 +101,14 @@ pub async fn send_confirmation_email(
 pub async fn insert_subscriber(
     pool: &PgPool,
     new_subscriber: &NewSubscriber,
-) -> Result<(), sqlx::Error> {
+) -> Result<(Uuid), sqlx::Error> {
+    let subsrciber_id = Uuid::new_v4();
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
         VALUES ($1, $2, $3, $4, 'pending_confirrmation')
         "#,
-        Uuid::new_v4(),
+        subsrciber_id,
         new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
@@ -112,5 +119,13 @@ pub async fn insert_subscriber(
         tracing::error!("Failed to execute query: {:?}", e);
         e
     })?;
-    Ok(())
+    Ok(subsrciber_id)
+}
+
+fn generate_subscription_token() -> String {
+    let mut rng = thread_rng();
+    std::iter::repeat_with(|| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(25)
+        .collect()
 }

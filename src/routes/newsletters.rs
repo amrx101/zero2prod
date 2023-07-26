e7bb048749e5a1bb96e348ad3945c804b2d44d9d
Amit Kumar <amit@turntide.com>
2023-07-26 20:18:39 +0530

    Argon2 storage

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,12 +1,15 @@
 use crate::domain::SubscriberEmail;
 use crate::email_client::EmailClient;
 use crate::routes::error_chain_fmt;
+use crate::telemetry::spawn_blocking_with_tracing;
 use actix_web::http::header::HeaderValue;
 use actix_web::http::{HeaderMap, HeaderValue, StatusCode};
 use actix_web::{web, HttpResponse, ResponseError};
 use anyhow::{anyhow, Context};
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use config::ValueKind::String;
 use reqwest::header::HeaderMap;
+use secrecy::Secret;
 use serde::de::Unexpected::Str;
 use sha3::Digest;
 use sqlx::PgPool;
@@ -57,7 +60,7 @@ impl ResponseError for PublishError {
 
 struct Credentials {
     username: String,
-    password: String,
+    password: Secret<String>,
 }
 
 fn basic_authenticate(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
@@ -89,28 +92,75 @@ fn basic_authenticate(headers: &HeaderMap) -> Result<Credentials, anyhow::Error>
     Ok(Credentials { username, password })
 }
 
+#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
+async fn get_stored_credentials(
+    username: &str,
+    pool: &PgPool,
+) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT user_id, password_hash
+        FROM users
+        WHERE username = $1
+        "#,
+        username,
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to performed a query to retrieve stored credentials.")?
+    .map(|row| (row.user_id, Secret::new(row.password_hash)));
+    Ok(row)
+}
+
 async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let password_hash = sha3::Sha3_256::digest(&credentials.password.as_bytes());
-    let password_hash = format!("{:x}", password_hash);
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+        gZiV/M1gPc22ElAH/Jh1Hw$\
+        CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
 
-    let user_id: Option<_> = sqlx::query!(
-        r#"
-            SELECT user_id FROM users WHERE username = $1 AND password_hash = S2
-        "#,
-        credentials.username,
-        password_hash
-    )
-    .fetch_optiona(pool)
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+
+    spawn_blocking_with_tracing(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
     .await
-    .context("FFailed to pefrom a query with creds")
-    .map_error(PublishError::UnexpectedError)?;
+    .context("Failed to spawn blocking task.")
+    .map_err(PublishError::UnexpectedError)??;
 
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(|| anyhow::anyhow!("Invalid username and password"))
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
+}
+
+#[tracing::instrument(
+    name = "Validate credentials",
+    skip(expected_password_hash, password_candidate)
+)]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_candidate: Secret<String>,
+) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
+        .context("Failed to parse hash in PHC string format.")
+        .map_err(PublishError::UnexpectedError)?;
+
+    Argon2::default()
+        .verify_password(
+            password_candidate.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("Invalid password.")
         .map_err(PublishError::AuthError)
 }
 
@@ -120,7 +170,6 @@ async fn validate_credentials(
     fields(username=tracing::field::Empty,
     user_id=tracing::field::Empty)
 )]
-
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
--- src/telemetry.rs
+++ src/telemetry.rs
@@ -1,16 +1,17 @@
+use tokio::task::JoinHandle;
 use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use tracing_log::LogTracer;
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-pub fn get_subscriber(
-    name: String,
-    env_filter: String
-) -> impl Subscriber + Send + Sync {
-    let env_filter = EnvFilter::try_from_default_env() .unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new( name,
-                                                       std::io::stdout );
-    Registry::default() .with(env_filter) .with(JsonStorageLayer) .with(formatting_layer)
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer)
 }
 /// Register a subscriber as global default to process span data.
 ///
@@ -20,4 +21,12 @@ pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
     set_global_default(subscriber).expect("Failed to set subscriber");
 }
 
+pub fn spawn_blocking_with_tracing<F, R>(f: F) -> JoinHandle<R>
+where
+    F: FnOnce() -> R + Send + 'static,
+    R: Send + 'static,
+{
+    let current_span = tracing::Span::current();
+    actix_web::rt::task::spawn_blocking(move || current_span.in_scope(f))
+}
 // TODO Understand this tracing set up.
